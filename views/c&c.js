ef405070e4fac3d3e33947de076397cd7d06660d
Evyatar Babay <evya61992@gmail.com>
2023-07-16 14:16:26 +0300

    new weather api fixed with images on landing page

--- views/c&c.js
+++ views/c&c.js
@@ -1,1 +1,18 @@
 const ws = new WebSocket("ws://localhost:8080");
+    ws.onopen = ()=>{
+    const gotLocation = (position)=>{
+        ws.send('&lat='+position.coords.latitude +'&lon=' +position.coords.longitude);
+   }
+   const noLocation = (err)=>{
+       ws.onopen = ()=> ws.send('&q=Tel Aviv');
+   }
+   navigator.geolocation.getCurrentPosition(gotLocation,noLocation);
+    }
+
+    ws.onmessage= event=>{
+        let wethData = JSON.parse(event.data);
+        $('.weather-icon').attr('src','images/'+wethData.weather[0].main+'.png') // TODO :: weather icon by weather details
+        $('.temp').text(wethData.main.temp) 
+        $('.city').text(wethData.name) 
+    }
+    
